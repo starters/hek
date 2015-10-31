d42e37190db3df945b012a7de4f518c3d0000f6a
E.Azer Koçulu <azer@kodfabrik.com>
2015-10-31 14:21:26 -0700

    Move starter to individual module

--- examples/npm-starter/index.js
+++ examples/npm-starter/index.js
@@ -1,6 +1,6 @@
 var struct = require("new-struct");
 var path = require("path");
-var Starter = require("../../").Starter;
+var Starter = require("kik-starter");
 
 var folder = path.join(__dirname, 'files');
 var form = [
@@ -8,6 +8,8 @@ var form = [
   { key: 'keywords', title: 'Keywords', desc: 'Separate with comma', commaList: true },
   { key: 'test', title: 'Test command', default: 'node test' }
 ];
+
+debugger;
 
 var NPMStarter = struct(Starter, {
   start: start
@@ -25,6 +27,8 @@ function NewNPMStarter (project) {
 }
 
 function start (starter, callback) {
+  debugger;
+
   starter.copy(folder, function (error) {
     starter.render(['README.md', 'package.json'], callback);
   });
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var NewProject = require('./lib/project');
-var NewStarter = require('./lib/starter');
 
 module.exports = NewProject;
-module.exports.Starter = NewStarter.Starter;
